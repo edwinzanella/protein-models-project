2f21f1067e24850facd0e8931565d6523e591fe8
edwinzanella <113215450+edwinzanella@users.noreply.github.com>
2026-06-24 12:36:33 -0600

    modified models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,21 @@
-# TODO
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class ActiveSiteGNN(nn.Module):
+    def __init__(self, in_channels, hidden_channels=64, num_layers=3, dropout=0.2):
+        super().__init__()
+        self.dropout = dropout
+        
+        self.convs = nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 1):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels))
+        
+        self.classifier = nn.Linear(hidden_channels, 2)
+        
+    def forward(self, x, edge_index):
+        for conv in self.convs:
+            x = F.relu(conv(x, edge_index))
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        return self.classifier(x)
